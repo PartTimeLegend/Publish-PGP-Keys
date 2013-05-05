9bc830a5f86b19297bab4ce917425fe2cadeba08
Antony Bailey <support@antonybailey.net>
2013-05-05 16:28:19 -0600

    All about trust

--- start.sh
+++ start.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
+sudo apt-get install -y
 gpg --import antonybailey.asc
+gpg --list-keys --with-colons --with-fingerprint `gpg --list-keys \
+--with-colons | grep "pub:f:.*:-:" | sed -r -e \
+'s/pub:f:[0-9]+:[0-9]+:([A-F0-9]+):.*/0x\1/'` | grep "^fpr:" \
+| sed -r -e 's/fpr:::::::::([0-9A-F]+):/\1:6:/' | gpg --import-ownertrust
+gpg --sign-key F2E50027 -y
 gpg --send-keys F2E50027
 gpg --recv-keys F2E50027
 gpg --refresh-keys
