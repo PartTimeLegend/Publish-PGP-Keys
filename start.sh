801e2f285f50db9d625613c6d476ea951b159813
Antony Bailey <support@antonybailey.net>
2021-01-25 14:47:01 +0000

    Update start.sh

--- start.sh
+++ start.sh
@@ -5,7 +5,7 @@ gpg --list-keys --with-colons --with-fingerprint `gpg --list-keys \
 --with-colons | grep "pub:f:.*:-:" | sed -r -e \
 's/pub:f:[0-9]+:[0-9]+:([A-F0-9]+):.*/0x\1/'` | grep "^fpr:" \
 | sed -r -e 's/fpr:::::::::([0-9A-F]+):/\1:6:/' | gpg --import-ownertrust
-gpg --sign-key F2E50027 -y
-gpg --send-keys F2E50027
-gpg --recv-keys F2E50027
+gpg --sign-key 9C02D726E567EF8B -y
+gpg --send-keys 9C02D726E567EF8B
+gpg --recv-keys 9C02D726E567EF8B
 gpg --refresh-keys
