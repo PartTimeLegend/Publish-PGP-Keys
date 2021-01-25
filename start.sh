9a6be4281342302cfe51cc03e4006bb2e9a044bb
Antony Bailey <support@antonybailey.net>
2021-01-25 14:51:41 +0000

    Update start.sh

--- start.sh
+++ start.sh
@@ -5,7 +5,7 @@ gpg --list-keys --with-colons --with-fingerprint `gpg --list-keys \
 --with-colons | grep "pub:f:.*:-:" | sed -r -e \
 's/pub:f:[0-9]+:[0-9]+:([A-F0-9]+):.*/0x\1/'` | grep "^fpr:" \
 | sed -r -e 's/fpr:::::::::([0-9A-F]+):/\1:6:/' | gpg --import-ownertrust
-gpg --sign-key 9C02D726E567EF8B -y
-gpg --send-keys 9C02D726E567EF8B
-gpg --recv-keys 9C02D726E567EF8B
-gpg --refresh-keys
+gpg --keyserver 'hkp://keyserver.ubuntu.com:11371' --sign-key 9C02D726E567EF8B -y
+gpg --keyserver 'hkp://keyserver.ubuntu.com:11371' --send-keys 9C02D726E567EF8B
+gpg --keyserver 'hkp://keyserver.ubuntu.com:11371' --recv-keys 9C02D726E567EF8B
+gpg --keyserver 'hkp://keyserver.ubuntu.com:11371' --refresh-keys
